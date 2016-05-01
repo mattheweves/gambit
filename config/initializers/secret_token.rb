2df89d400d141f86361699222283d4d7b0fc08f3
Matt Eves <mattheweves@outlook.com>
2016-05-01 18:38:31 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gambit::Application.config.secret_key_base = '97bea67bc33a4a1d51fccb6a0c0d0385f3aed29182b060dbb6773ccddb1e0c7476b46cd17bd97c1785bb647babd31f378ee1590f86bba32b6c9c8a7b3fcf5ef9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gambit::Application.config.session_store :cookie_store, key: '_gambit_session'
